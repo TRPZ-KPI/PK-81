1572c31e4e9e483a479ddba0ed53eba9b081fdea
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-10-28 10:36:19 +0200

    add lab4

---
+++ lab4/Student.java
@@ -0,0 +1,49 @@
+package lab4;
+
+public class Student {
+    private String name;
+    private String surname;
+    private String lastName;
+    private int age;
+    private int course;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+}
