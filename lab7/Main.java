504163ec5e9240ab1dd1e602b32abcd8e8654091
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2020-02-04 10:38:49 +0200

    add lab7

---
+++ lab7/Main.java
@@ -0,0 +1,10 @@
+package lab7;
+
+public class Main {
+    public static void main(String[] args) {
+        MySet mySet = new MySet();
+        System.out.println(mySet.size());
+        mySet.add(1);
+        System.out.println(mySet.size());
+    }
+}
